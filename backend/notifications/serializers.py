19da83102f6e95b4e44c8eba2477e664681fdf92
Badmus <temitopebdms@gmail.com>
2022-08-29 12:56:55 -0700

    Update notification serializer

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,12 +1,11 @@
 from home.models import Notification
 from rest_framework import serializers
+from users.serializers import UserSerializer
 
 
 class NotificationSerializer(serializers.ModelSerializer):
     sender_name = serializers.CharField(source="from_user.name", read_only=True)
-    sender_avatar = serializers.URLField(
-        source="from_user.profile_picture", read_only=True
-    )
+    sender_avatar = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
@@ -19,3 +18,6 @@ class NotificationSerializer(serializers.ModelSerializer):
             "created_at",
         )
         read_only_fields = ("redirect_url", "verb")
+
+    def get_sender_avatar(self, obj):
+        return UserSerializer(obj.from_user).data["profile_picture"]
